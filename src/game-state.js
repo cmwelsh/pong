6fa161b867415304730dd5b3c932d3fe6df24ca7
cmwelsh <chris@cmwelsh.com>
2014-05-27 11:36:49 -0700

    Recalibrate ball position when resizing board

--- src/game-state.js
+++ src/game-state.js
@@ -11,8 +11,8 @@ function GameState() {
         y: 50
       },
       velocity: {
-        x: 1,
-        y: 1
+        x: Math.round(Math.random()) ? 1 : -1,
+        y: Math.round(Math.random()) ? 1 : -1
       },
       radius: 3
     },
@@ -53,15 +53,26 @@ GameState.prototype.getScore = function() {
 };
 
 GameState.prototype.resize = function(options) {
+  var newWidth, newHeight;
+  var oldWidth = this._state.board.width;
+  var oldHeight = this._state.board.height;
+
+  // Set "virtual" board width and height based on whichever value is larger
   if (options.width > options.height) {
-    this._state.board.width = options.width;
-    this._state.board.height = options.height;
+    newWidth = options.width;
+    newHeight = options.height;
     this._state.board.orientation = 0;
   } else {
-    this._state.board.height = options.width;
-    this._state.board.width = options.height;
+    newWidth = options.height;
+    newHeight = options.width;
     this._state.board.orientation = 90;
   }
+  this._state.board.width = newWidth;
+  this._state.board.height = newHeight;
+
+  // Recalibrate ball position based on old board size
+  this._state.ball.position.x *= newWidth / oldWidth;
+  this._state.ball.position.y *= newHeight / oldHeight;
 };
 
 window.pong.GameState = GameState;
